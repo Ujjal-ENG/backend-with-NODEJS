c8213783cc13d41e9048d345f8e53c7130b4a4b9
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-14 20:00:16 +0600

    successfully running the server

--- node-toutorial/index.js
+++ node-toutorial/index.js
@@ -1,6 +1,11 @@
-// dependencies
-const http = require('http');
-// app object - module scaffolding
-const app = {};
-// configuration
-app.config = {};
+/* eslint-disable import/no-extraneous-dependencies */
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+app.listen(3000, () => {
+    console.log(`Server is running at ${3000}`);
+});
