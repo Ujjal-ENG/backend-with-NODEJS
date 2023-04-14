1940a452d232b9b8b7dc71ea594682cbadd571b4
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-15 01:05:54 +0600

    done the mongodb conntection

--- node-toutorial/index.js
+++ node-toutorial/index.js
@@ -1,8 +1,20 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+app.use(express.json());
 
+// connection code for mongoDB using mongoose
+mongoose
+    .connect('mongodb+srv://ujjal:J4dyr2YSsN2XJbcQ@cluster0.n44zgvg.mongodb.net/todos', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log('Connected');
+    })
+    .catch((err) => console.log(err));
 app.get('/', (req, res) => {
     res.send('hello there from Home Page');
 });
