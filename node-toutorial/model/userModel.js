319e4d76e8c10390f1a223a5c94e9776ac9685fb
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-15 20:45:41 +0600

    for login purpose maked the userSchema

---
+++ node-toutorial/model/userModel.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const UserSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive'],
+    },
+});
+
+export default UserSchema;
