d22245774909cc1f59b40a275688680b1e396b31
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-19 23:50:21 +0600

    successfully created the data

--- rest-api-crud/controllers/usersControllers.js
+++ rest-api-crud/controllers/usersControllers.js
@@ -1,3 +1,36 @@
+const users = [
+  {
+    firstName: "Ujjal",
+    lastName: "Roy",
+    age: 23
+  },
+  {
+    firstName: "Ador",
+    lastName: "Roy",
+    age: 22
+  }
+]
+
 export const getAllUsers = (req, res) => {
-  res.send("Hello from huj routes")
+  res.status(200).json({
+    msg: "Data is Successfully get",
+    results: users.length,
+    data: users
+  })
+}
+export const createUser = async (req, res) => {
+  try {
+
+    const data = await (req.body)
+
+    res.status(201).json({
+    msg: "Data is Successfully created",
+    results: users.length,
+    data: users.push(data)
+  })
+  } catch (error) {
+    res.status(201).json({
+      msg:error,
+    })
+  }
 }
--- rest-api-crud/routes/usersRoutes.js
+++ rest-api-crud/routes/usersRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllUsers } from "../controllers/usersControllers.js";
+import { createUser, getAllUsers } from "../controllers/usersControllers.js";
 
 const router = express.Router()
 
 router.get("/", getAllUsers)
+router.post("/", createUser)
 
 
 export default router
