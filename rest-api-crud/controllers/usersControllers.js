510f3e8ee0708c3a6afa85ed0a1a166a69117267
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-19 23:58:16 +0600

    done this app

--- rest-api-crud/controllers/usersControllers.js
+++ rest-api-crud/controllers/usersControllers.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from 'uuid'
 const users = [
   {
     firstName: "Ujjal",
@@ -22,7 +23,23 @@ export const createUser = async (req, res) => {
   try {
 
     const data = await (req.body)
+    const userId = uuidv4()
+    res.status(201).json({
+    msg: "Data is Successfully created",
+    results: users.length,
+    data: users.push({...data,id:userId})
+  })
+  } catch (error) {
+    res.status(201).json({
+      msg:error,
+    })
+  }
+}
+export const findUserById = async (req, res) => {
+  try {
 
+    const id = await (req.body.id)
+console.log(id)
     res.status(201).json({
     msg: "Data is Successfully created",
     results: users.length,
--- rest-api-crud/routes/usersRoutes.js
+++ rest-api-crud/routes/usersRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { createUser, getAllUsers } from "../controllers/usersControllers.js";
+import { createUser, getAllUsers,findUserById } from "../controllers/usersControllers.js";
 
 const router = express.Router()
 
 router.get("/", getAllUsers)
 router.post("/", createUser)
+router.get("/:id", findUserById)
 
 
 export default router
