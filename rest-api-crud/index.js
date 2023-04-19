d3a6870ac80c0a8598e494efe5a455adad7e991b
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-19 23:39:18 +0600

    made our own controllers

--- rest-api-crud/index.js
+++ rest-api-crud/index.js
@@ -1,9 +1,11 @@
 import bodyParser from "body-parser";
 import express from "express";
-
+import usersRoutes from './routes/usersRoutes.js';
 const app = express()
 const PORT = 5000;
 app.use(bodyParser.json())
+
+app.use("/users",usersRoutes)
 
 app.get("/", (req, res) => {
   res.json({msg:"mama Kopa"})
---
+++ rest-api-crud/routes/usersRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { getAllUsers } from "../controllers/usersControllers.js";
+
+const router = express.Router()
+
+router.get("/", getAllUsers)
+
+
+export default router
