a9f365d5325750cf9b256784d931e0a487292951
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-19 23:31:48 +0600

    server run and worked it very perfectly

--- rest-api-crud/index.js
+++ rest-api-crud/index.js
@@ -2,11 +2,12 @@ import bodyParser from "body-parser";
 import express from "express";
 
 const app = express()
-
 const PORT = 5000;
+app.use(bodyParser.json())
 
-
-app.unsubscribe(bodyParser.json())
+app.get("/", (req, res) => {
+  res.json({msg:"mama Kopa"})
+})
 
 
 app.listen(PORT, () => {
