c2123bf53dd502570d0ef8f0e54b49f3384b31d9
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-06-18 20:27:48 +0600

    server is created successfully

--- learn-express/index.js
+++ learn-express/index.js
@@ -1,6 +1,25 @@
-// dependencies
-const http = require('http');
-// app object - module scaffolding
-const app = {};
-// configuration
-app.config = {};
+import cors from 'cors';
+import dotenv from 'dotenv';
+import express from 'express';
+import morgan from 'morgan';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// default middleware
+app.use(express.json());
+app.use(cors());
+app.use(morgan('dev'));
+
+// default route
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'This is a deafult route!!!' });
+});
+
+// listen route
+app.listen(PORT, () => {
+    console.log('Server is running at 8080 PORT');
+});
