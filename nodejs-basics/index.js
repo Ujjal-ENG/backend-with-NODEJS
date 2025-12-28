055ada2ce9e6c3714032fa18e2d083fb6a4f9f41
ujjal <ujjalroy7862@gmail.com>
2025-12-28 13:26:07 +0600

    feat: forget about the require in node alaways focus the import and export module js

--- nodejs-basics/index.js
+++ nodejs-basics/index.js
@@ -1,7 +1,3 @@
-const startTime = process.hrtime();
-for (let i = 0; i < 1e6; i++) {
-  // Simulate some work
-  Math.sqrt(i);
-}
-const endTime = process.hrtime(startTime);
-console.log(`Execution time: ${endTime[0]}s ${endTime[1] / 1e6}ms`);
+import os from 'os';
+console.log(`this is my os information ${os.platform()} and this is my hostname: ${os.hostname()}`)
+
