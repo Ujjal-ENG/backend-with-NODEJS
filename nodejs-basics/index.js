871488184b851ac47a06db58051b8c4e04e0e2f4
ujjal <ujjalroy7862@gmail.com>
2025-12-28 17:36:06 +0600

    feature: performance increased the index.js for one thread in js

--- nodejs-basics/index.js
+++ nodejs-basics/index.js
@@ -1,9 +1,30 @@
 import http from 'http'
+import queryString from 'querystring'
+import url from 'url'
 
 http.createServer((req, res) => {
-  console.log(req.url)
-  res.write("Hello World")
-  res.end()
-}).listen(3000)
 
-console.log(`server lisitening on http://localhost:${3000}`)
+  res.write("Your request is being processed.... \n")
+  const query = url.parse(req.url).query
+  const n = Number(queryString.parse(query)['n'])
+  findSum(n, (sum) => {
+   res.end(`The sum is: ${sum} \n`)
+ })
+  
+}).listen(3000, () => {
+  console.log(`Server is listening on http://localhost:${3000}`)
+})
+
+
+function findSum(n, sumCallBack) {
+  let sum = 0;
+  function add(i, cb) {
+    sum += i
+    if (i == n) {
+      return cb(sum)
+    }
+    setImmediate(add.bind(null,i+1,cb))
+  }
+
+  add(1,sumCallBack)
+}
